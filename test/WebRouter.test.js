5b122fa7154b68da3a192487da59acb97200a218
Leo Brad <>
2025-05-10 18:37:34 +0800

    [Add] Added support for restoring all characters.

--- test/WebRouter.test.js
+++ test/WebRouter.test.js
@@ -10,6 +10,7 @@ describe('[Class] WebRouter;', () => {
       dutyCycle: 5,
       logLevel: 7,
       logInterval: 5,
+      interception: undefined,
     });
     webRouter.add('/movie/action//{page}/{index}', ['Thunderbolts', 'Sinners', 'Havoc']);
     expect(JSON.stringify(webRouter.root.find('movie').find('action').pathKeys)).toMatch('[\"page\",\"index\"]');
@@ -23,6 +24,7 @@ describe('[Class] WebRouter;', () => {
       dutyCycle: 5,
       logLevel: 7,
       logInterval: 5,
+      interception: undefined,
     });
     webRouter.add('/movie/action//{page}/{index}', ['Thunderbolts', 'Sinners', 'Havoc']);
     expect(JSON.stringify(webRouter.match('/movie/action//1/1?k1=v1&k2=v2'))).toMatch('{\"content\":[\"Thunderbolts\",\"Sinners\",\"Havoc\"],\"queryParams\":{\"k2\":\"v2\",\"k1\":\"v1k2\"},\"pathVariables\":{\"page\":\"1\",\"index\":\"1\"}}');
@@ -36,6 +38,7 @@ describe('[Class] WebRouter;', () => {
       dutyCycle: 5,
       logLevel: 7,
       logInterval: 5,
+      interception: undefined,
     });
     webRouter.add('/movie/action//{page}/{index}', ['Thunderbolts', 'Sinners', 'Havoc']);
     webRouter.setPathKeys('/movie/action//{start}/{end}');
