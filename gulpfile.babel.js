0f3341c783c994c47f9913bc90a138a018bb2dc7
Leo Ely <leoely789@gmail.com>
2025-09-21 21:47:00 +0800

    [Add] Add error log information.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,9 @@
 import { series, src, dest, } from 'gulp';
 import babel from 'gulp-babel';
-import minify from 'gulp-babel-minify';
 
 function build() {
   return src('src/**/*.js')
     .pipe(babel())
-    .pipe(minify({
-      mangle: {
-        keepClassName: true
-      }
-    }))
     .pipe(dest('dist'));
 }
 
